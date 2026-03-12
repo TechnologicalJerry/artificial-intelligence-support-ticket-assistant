9783f223b16adeea1bfacc2541ae4d6bd41bcc4d
Rajat Verma <cserajatverma@gmail.com>
2026-03-12 23:24:38 +0530

    Use relative env import in mysql pool
    Replace the aliased import '@/config/env.js' with a relative import '../config/env.js' in fastify-server/src/db/mysql.ts. This fixes module resolution issues where the path alias isn't recognized at runtime/build for the MySQL pool initialization.

--- fastify-server/src/db/mysql.ts
+++ fastify-server/src/db/mysql.ts
@@ -1,5 +1,5 @@
 import mysql from 'mysql2/promise';
-import { env } from '@/config/env.js';
+import { env } from '../config/env.js';
 
 export function createMySqlPool() {
   return mysql.createPool({
